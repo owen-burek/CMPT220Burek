443383eeaabc5d81df95220f47b2af0ad84067cd
Owen Burek <Owen.Burek1@marist.edu>
2017-03-01 20:55:31 -0500

    mend

--- labs/3/Problem5_1.java
+++ labs/3/Problem5_1.java
@@ -1,7 +1,7 @@
 /*
 Owen Burek
 February 23, 2017
-Lab 2 Problem 3.8
+Lab 3 Problem 5.1
 */
 
 import java.util.*;
--- labs/3/Problem5_7.java
+++ labs/3/Problem5_7.java
@@ -1,7 +1,7 @@
 /*
 Owen Burek
 February 23, 2017
-Lab 2 Problem 3.8
+Lab 3 Problem 5.7
 */
 
 import java.util.*;
