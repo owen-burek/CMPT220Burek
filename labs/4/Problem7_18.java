486edd380570c4ebd3b45e47c76a539b463ad9a5
Owen Burek <Owen.Burek1@marist.edu>
2017-03-08 19:21:50 -0500

    Owen Burek, 2/8/17. Finished Problem 7.18

--- labs/4/Problem7_18.java
+++ labs/4/Problem7_18.java
@@ -11,36 +11,31 @@ import java.util.*;
 public class Problem7_18{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-    double[] array = new double[10];
-    double[] bubbleArray = bubbleSort(array); 
+    double[] array = new double[10]; 
     
     System.out.println("Please enter ten numbers: ");
     for(int i = 0; i <= 9; i++)
       array[i] = input.nextDouble();
 
+    double[] bubbleArray = bubbleSort(array);
+
     System.out.println("The 10 elements in ascending order: ");
     for (int k = 0; k <= 9; k++)
-      System.out.print(array[k] + " ");
+      System.out.print(bubbleArray[k] + " ");
   }
   
 
-  public static double[] bubbleSort(double[] array){
-    double temp = 0;
+  public static double[] bubbleSort(double[] arraySort){
+    double temp = 0.0;
     for(int i = 0; i <= 9; i++){
-      for (int k = 0; k < 9; k++){
-        if (array[k] > array[k+1])
-          temp = array[k];
-          array[k] = array[k+1];
-          array[k+1] = temp;
+      for (int k = 0; k <= 8; k++){
+        if (arraySort[k] > arraySort[k+1]){
+          temp = arraySort[k];
+          arraySort[k] = arraySort[k+1];
+          arraySort[k+1] = temp;
+        }
       }   
     }
-    return array;
-  }
-
-
-  static void swapElements(double[] array, int index1, int index2){
-    double temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
+    return arraySort;
   }
 }
